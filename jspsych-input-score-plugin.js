1870dd5ba1d2aafaef9bcc348e73ca3e6f6bc1ad
Mariela Jennings <marielajennings@gmail.com>
2017-09-01 17:13:49 -0400

    now you can input a score

---
+++ jspsych-input-score-plugin.js
@@ -0,0 +1,44 @@
+
+function definePlugin (){
+var plugin = {};
+
+plugin.info= {
+	name: 'visualize-results',
+	description:'This plugin makes your results more visually appealing!',
+	parameters: {
+	participant_score: {
+        type: [jsPsych.plugins.parameterType.INTEGER],
+        default: undefined,
+        no_function: false,
+        description: 'This is the score the participant got.'
+      }
+
+	}
+
+}
+
+plugin.trial= function (display_element, trial) {
+
+
+
+
+var p = document.createElement('p');
+display_element.appendChild(p);
+p.innerHTML="Your score: "+trial.participant_score+"/32"
+var div= document.createElement('div');
+div.setAttribute('style','background-color:#ddd; width: 100%; height:40px; border-radius: 25px;');
+display_element.appendChild(div);
+var div2 = document.createElement('div');
+div2.setAttribute ('style','width:50%; background-color:#00bcd4; border:00a5bb; text-align: right; padding-right: 20px; line-height: 40px; color: white; height:40px; border-radius: 25px;');
+div.appendChild(div2);
+
+
+
+
+
+}
+return plugin;
+}
+
+jsPsych.plugins['visualize-results'] = definePlugin() ;
+
